e6497d7f78db698f4aedd015a88a097a0c79ffe2
cj-prince <davids.chijioke@gmail.com>
2021-09-03 03:51:36 +0100

    display and adding category button manuely

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,34 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+const [menuItems, setMenuItems] = useState(items)
+const [categories, setCategories] = useState([])
+
+const filterItems = (category) => {
+  const newItems = items.filter((item) => item.category === category)
+  setMenuItems(newItems)
+};
+  return <main>
+    <section className='menu section'>
+      <div className='title'>
+        <h2>Our Menu</h2>
+         <button className='underline'></button>
+      </div>
+      <Categories filterItem = {filterItems}/>
+       < Menu items={menuItems}/>
+    </section>
+      
+     
+      {/* <section>
+        {display.map ((display) =>{
+          return < Menu key = {display.id} {...display}/>
+        })}
+        
+      </section> */}
+     
+    </main>
+    
+  ;
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItem}) => {
+  return <div className="btn-container">
+    <button className='filter-btn' onClick={()=> filterItem('breakfast')}>breakfast</button>
+    <button className='filter-btn' onClick={()=> filterItem('lunch')}>lunch</button>
+  </div>
 };
 
 export default Categories;
