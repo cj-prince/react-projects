b6d1d20f7ce6089dc8cdc0cbf344aeef2ffffd8a
cj-prince <davids.chijioke@gmail.com>
2021-09-10 12:46:21 +0100

    basic stucture

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,29 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+const [name, setName] = useState('');
+const [list, setList] = useState([]);
+const [isEditing, setIsEditing] = useState(false);
+const [alert, setAlert] = useState({show:false, msg:'', type: ''});
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return <section className='section-center'>
+    <form className='grocery-form' onChange={handleSubmit}>
+      {alert.show && <Alert />}
+      <h3>grocery bub</h3>
+      <div className="form-control">
+        <input type="text" className='grocery' placeholder='eg. beans' 
+         value={name} onChange={(e)=> setName(e.target.value)}/>
+        <button className='submit-btn' type='submit'>{isEditing? 'edit' : 'submit'}</button>
+      </div>
+    </form>
+    <div className='grocery-container'>
+    <List />
+    <button className='clear-btn'>Clear items</button>
+    </div>
+  </section>
 }
 
 export default App
