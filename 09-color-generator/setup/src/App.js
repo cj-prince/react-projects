7b4e5bc6df77e4adcff4f1c9d0db6a8b4712ad43
cj-prince <davids.chijioke@gmail.com>
2021-09-10 02:48:57 +0100

    onsreen default color placing, copy to clip and timeout

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#222').all(5));
   const handleSubmit = (e) => {
     e.preventDefault();
     try{
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,11 +5,22 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const [alert, setAlert] = useState(false);
   const ccc = rgb.join(',')
   const hex = rgbToHex(...rgb)
+  useEffect(()=>{
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    },2000)
+    return() => clearTimeout(timeout)
+  },[alert])
   return <article className={'color'} style={
-    {backgroundColor:`rgb(${ccc})`}}>
+    {backgroundColor:`rgb(${ccc})`}}
+    onClick={() => {
+      setAlert(true); navigator.clipboard.writeText(hex)
+    }}
+    >
       <p className='percent-value'>{weight}%</p>
       {/* <p className='color-value'>{hex}</p> */}
-      <p className='color-value'>{hexColor}</p>
+      <p className='color-value'>#{hexColor}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
       </article>
 }
 
