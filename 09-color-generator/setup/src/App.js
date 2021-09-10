bb60ca35320778ff3aceba1a4df70594edf4860b
cj-prince <davids.chijioke@gmail.com>
2021-09-10 01:47:11 +0100

    basic form setup

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,26 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  } 
+  return <>
+    <section className='container'>
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={color}
+         onChange={(e) => setColor(e.target.value)} placeholder='#f15025' />
+         <button type='submit' >Submit</button>
+      </form>
+    </section>
+    <section>
+      <p>list go here</p>
+    </section>
+  </>
 }
 
 export default App
