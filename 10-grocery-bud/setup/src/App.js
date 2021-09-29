c6dcf9c42d30420c41f092dbc25f070a1473b0c8
cj-prince <davids.chijioke@gmail.com>
2021-09-29 02:14:00 +0100

    alert functions and clear list function

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -7,26 +7,35 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({show:true, msg:'hello', type: 'success'});
+  const [alert, setAlert] = useState({show:false, msg:'', type: ''});
   const handleSubmit = (e) => {
     e.preventDefault()
     if(!name){
       // display alert
+      showAlert(true, "danger", "enter a value")
     }
     else if(name && isEditing){
       // deal with edit
     }
     else {
       // show alert
+      showAlert(true,'success','item successfully addedd')
       const newItem = {id: new Date().getTime().toString(), title:name};
       setList([...list,newItem])
       setName('')
     }
   }
-
+  const showAlert=(show=false, type='', msg='')=>{
+    setAlert({show, type, msg})
+  }
+  const clearList=()=>{
+    showAlert(true,'danger','empty list')
+    setList([])
+  }
+  
   return <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
-      {alert.show && <Alert {...alert} />}
+      {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
       <h3>grocery bub</h3>
       <div className="form-control">
         <input type="text" className='grocery' placeholder='eg. beans' 
@@ -37,7 +46,7 @@ function App() {
     {list.length>0 && (
        <div className='grocery-container'>
       <List items={list}/>
-      <button className='clear-btn'>Clear items</button>
+      <button className='clear-btn'onClick={clearList}>Clear items</button>
     </div>
     )}
    
