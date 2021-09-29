0223e68533767f51ff4d946fa93d1718a5432d21
cj-prince <davids.chijioke@gmail.com>
2021-09-29 02:23:15 +0100

    delete function

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -32,7 +32,10 @@ function App() {
     showAlert(true,'danger','empty list')
     setList([])
   }
-  
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item)=>item.id !==id))
+  }
   return <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
       {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
@@ -45,7 +48,7 @@ function App() {
     </form>
     {list.length>0 && (
        <div className='grocery-container'>
-      <List items={list}/>
+      <List items={list} removeItem={removeItem}/>
       <button className='clear-btn'onClick={clearList}>Clear items</button>
     </div>
     )}
