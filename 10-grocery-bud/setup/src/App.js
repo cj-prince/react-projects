d8d02e4b25075c31c0cc0251717a2aa43a6a24c7
cj-prince <davids.chijioke@gmail.com>
2021-09-29 02:41:59 +0100

    edit function

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -16,6 +16,16 @@ function App() {
     }
     else if(name && isEditing){
       // deal with edit
+      setList(list.map((item)=>{
+        if(item.id === editID) {
+          return {...item, title:name}
+        }
+        return item
+      }))
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'value changed' )
     }
     else {
       // show alert
@@ -36,9 +46,15 @@ function App() {
     showAlert(true, 'danger', 'item removed')
     setList(list.filter((item)=>item.id !==id))
   }
+  const editItem = (id) => {
+    const specificItem = list.find((item)=> item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setName(specificItem.title)
+  }
   return <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
-      {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+      {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}
       <h3>grocery bub</h3>
       <div className="form-control">
         <input type="text" className='grocery' placeholder='eg. beans' 
@@ -48,7 +64,7 @@ function App() {
     </form>
     {list.length>0 && (
        <div className='grocery-container'>
-      <List items={list} removeItem={removeItem}/>
+      <List items={list} removeItem={removeItem} editItem={editItem}/>
       <button className='clear-btn'onClick={clearList}>Clear items</button>
     </div>
     )}
