ff06f63d4d976bd18c5fb9937026de07858dccb5
cj-prince <davids.chijioke@gmail.com>
2021-09-29 01:22:13 +0100

    displaying input

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,16 +3,29 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-const [name, setName] = useState('');
-const [list, setList] = useState([]);
-const [isEditing, setIsEditing] = useState(false);
-const [alert, setAlert] = useState({show:false, msg:'', type: ''});
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show:false, msg:'', type: ''});
   const handleSubmit = (e) => {
     e.preventDefault()
+    if(!name){
+      // display alert
+    }
+    else if(name && isEditing){
+      // deal with edit
+    }
+    else {
+      // show alert
+      const newItem = {id: new Date().getTime().toString(), title:name};
+      setList([...list,newItem])
+      setName('')
+    }
   }
 
   return <section className='section-center'>
-    <form className='grocery-form' onChange={handleSubmit}>
+    <form className='grocery-form' onSubmit={handleSubmit}>
       {alert.show && <Alert />}
       <h3>grocery bub</h3>
       <div className="form-control">
@@ -22,7 +35,7 @@ const [alert, setAlert] = useState({show:false, msg:'', type: ''});
       </div>
     </form>
     <div className='grocery-container'>
-    <List />
+    <List items={list}/>
     <button className='clear-btn'>Clear items</button>
     </div>
   </section>
