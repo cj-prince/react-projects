ce3f4156762764db2c0a6b97b41439afef8cb8ec
cj-prince <davids.chijioke@gmail.com>
2021-12-08 17:21:54 +0100

    dynamic setup

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -15,16 +15,21 @@ const Navbar = () => {
         </div>
         <div className="links-container show-container">
           <ul className="links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">contact</a></li>
-            <li><a href="#">products</a></li>
+            {links.map((link) =>{
+              const {id, url, text} = link 
+              return (
+                <li key={id}><a href={url}>{text}</a></li>
+              )
+            })}
           </ul>
         </div>
         <ul className="social-icons">
-          <li><a href="https://www.twitter.com"><FaTwitter/></a></li>
-          <li><a href="https://www.twitter.com"><FaFacebook/></a></li>
-          <li><a href="https://www.twitter.com"><FaTwitter/></a></li>
+            {social.map((social) =>{
+              const {id, url, icon} = social
+              return (
+                <li key={id}><a href={url}>{icon}</a></li>
+              )
+            })}
         </ul>
 
       </div>
