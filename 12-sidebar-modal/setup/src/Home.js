c8c1785f4a9af5059432274a483ca750f3556bc3
cj-prince <davids.chijioke@gmail.com>
2021-12-13 12:43:30 +0100

    usecontext ex

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -2,13 +2,13 @@ import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
 import { AppContext, useGlobalContext } from './context'
 const Home = () => {
-  const data = useGlobalContext
+  const {openSidebar, openModal} = useGlobalContext()
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars/>
       </button>
-      <button className="btn">Show Modal</button>
+      <button className="btn" onClick={openModal}>Show Modal</button>
     </main>
   )
 }
