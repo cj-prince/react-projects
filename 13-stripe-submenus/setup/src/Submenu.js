231145ed99ae98eb3e82a5f83963a419e558507a
cj-prince <davids.chijioke@gmail.com>
2021-12-16 04:54:18 +0100

    hooks

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,38 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
+  const {isSubmenuOpen, location, page: {page, links}} = useGlobalContext()
+  const container = useRef(null)
+  const [column, setColumn] = useState('col-2') 
+  useEffect(() =>{
+    setColumn('col-2')
+    const submenu = container.current
+    const {center, bottom} = location
+    submenu.style.left = `${center}px`
+    submenu.style.top = `${bottom}px`
+
+    if (links.length === 3) {
+      setColumn('col-3')
+    }
+    if (links.length > 3){
+      setColumn('col-4')
+    }
+  },[location, links])
+  return <aside className={`${isSubmenuOpen ? "submenu show":"submenu"}`} ref={container}>
+   <h4>{page}</h4>
+   <div className={`submenu-center ${column}`}>
+    {links.map((links, index)=>{
+      const {label, icon,url} = links
+      return (
+        <a key={index} href={url}>
+          {icon}
+          {label}
+        </a>
+      )
+    })}
+   </div>
+  </aside>
 }
 
 export default Submenu
