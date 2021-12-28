c7dca1cbb188d9c5b529e6c9cd519b98825ae07e
cj-prince <davids.chijioke@gmail.com>
2021-12-28 16:06:15 +0100

    clear function using reducer

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -16,10 +16,14 @@ const InitialState = {
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer,InitialState)
 
+  const clearCart = () => {
+    dispatch({type: 'CLEAR_CART'})
+  }
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
       }}
     >
       {children}
