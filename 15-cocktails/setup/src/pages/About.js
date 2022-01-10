89342d0da89f998d036471a9f0bc9814a2c6170e
cj-prince <davids.chijioke@gmail.com>
2022-01-10 14:29:34 +0100

    router,switch,link,nav and error page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Exercitationem cupiditate sapiente architecto iste perferendis 
+        nisi ipsa dolore consequuntur laborum obcaecati animi voluptatum praesentium,
+        quis accusamus, deserunt pariatur, et voluptatem est!</p>
+    </section>
   )
 }
 
